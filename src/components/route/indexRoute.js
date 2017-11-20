1d6fa328f07e600285b5f544f2478ead6b7aed6a
zhaoshuai3 <zhaoshuai3@xin.com>
2017-11-20 19:57:42 +0800

    增加树形菜单并使用路由

--- src/components/route/indexRoute.js
+++ src/components/route/indexRoute.js
@@ -1,17 +1,85 @@
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import {Route,BrowserRouter as Router,Link,Switch} from 'react-router-dom';
-import HeaderRoute from './headerRoute';
-import MenuRoute from './menuRoute';
+import { Tree } from 'antd';
+const TreeNode = Tree.TreeNode;
 
-const Group = ({match:{params}}) => {
+const noteTrees = [
+    {
+        'name':'java',
+        'id':1,
+        'isFile':0,
+        'subItems':[
+            {'name':'java基础',
+            'id':11,
+            'isFile':0,
+            'subItems':[{
+                'name':'集合',
+                'id':111,
+                'isFile':1
+                }
+            ]}
+        ]
+    },{
+        'name':'react',
+        'id':2,
+        'isFile':0,
+        'subItems':[
+            {'name':'rn',
+            'id':21,
+            'isFile':1,
+            'subItems':[
+                
+            ]}
+        ]
+    }
+]
+const loadTreeNodes = (data,group) => {
+    return data.map((item) => {
+      if (item.isFile != 1) {
+        return (
+          <TreeNode title={item.name} key={item.id} dataRef={item}>
+            {loadTreeNodes(item.subItems,group)}
+          </TreeNode>
+        );
+      }
+      var linkTo = '/menu/' + group +"/" + item.id;
+      let title = (
+          <Link to={linkTo}>{item.name}</Link>
+      );
+      return (
+        <TreeNode title={title} key={item.id}  dataRef={item} >
+            </TreeNode>);
+    });
+  }
+const Group = ({match}) => {
     // 请求日志对应的路由信息，引用第三方组件,带分组框
-    let group = params.group;
+    let group = match.params.group;
+    if('note' == group){
+        return (
+            <Tree showLine onSelect={(selectedKeys, info) => {
+                let selectedKey = selectedKeys[0];
+            }} >
+                {loadTreeNodes(noteTrees,group)}
+            </Tree>
+        );
+    }else{
+        return (
+            <Tree onSelect={(selectedKeys, info) => {
+                let selectedKey = selectedKeys[0];
+            }}>
+                <TreeNode title='user' key='user' />
+            </Tree>
+        );
+    }
+    
 }
 // 详情页的路由以detail为开头，后面跟着父组件id和详情的id
 const Detail = ({match:{params}}) => {
-    let parentId = params.parentId;
     let detailId = params.detailId;
+    return (
+        <div>{detailId}</div>
+    );
 }
 // 修改右键的默认行为，增加添加功能，添加时时候先调接口创建一个空文档，并且返回对应id，在已有的路由里面添加对应的路由
 class IndexRouter extends Component{
@@ -20,22 +88,24 @@ class IndexRouter extends Component{
             <div>
                 <Router>
                     <div>
-                        <ul>
-                            <li><Link to="/menu/note">note</Link></li>
-                            <li><Link to='/menu/user'>user</Link></li>
-                        </ul>
-                    </div>
-                    <div>
                         <div>
-                            <Switch>
-                                <Route exact path='/' component={}/>
-                                <Route path='/menu/:group' Component={Group}></Route>
-                            </Switch>
+                            <ul>
+                                <li><Link to="/menu/note">note</Link></li>
+                                <li><Link to='/menu/user'>user</Link></li>
+                            </ul>
                         </div>
                         <div>
-                        <Switch>
-                                <Route path='/detail/:parentId/:detailId' Component={Detail}></Route>
+                            <div>
+                                <Switch>
+                                    <Route exact path='/' component={Group}/>
+                                    <Route path='/menu/:group' component={Group}></Route>
+                                </Switch>
+                            </div>
+                            <div>
+                            <Switch>
+                                    <Route path='/menu/:group/:detailId' component={Detail}></Route>
                             </Switch>
+                            </div>
                         </div>
                     </div>
                 </Router>
@@ -43,3 +113,5 @@ class IndexRouter extends Component{
         );
     }
 }
+
+export default IndexRouter;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import NoteAdd from './components/note/note_add'
+import IndexRoute from './components/route/indexRoute'
 
-ReactDOM.render(<NoteAdd parentDir='java'/>, document.getElementById('root'));
+ReactDOM.render(<IndexRoute parentDir='java'/>, document.getElementById('root'));
 registerServiceWorker();
